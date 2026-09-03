1645bbc745e58e407b0b7f30c1056cb4bcfc16e5
motivenation885-sudo <motivenation885@gmail.com>
2026-09-03 18:07:38 +0530

    new app.js updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,12 @@
+import Script from "next/script";
 import "../styles/globals.css";
 import { CartProvider } from "../lib/cart";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
       <Component {...pageProps} />
-
-      <Script
-        src="https://c36afef3-bd1c-41d8-9c28-4cc63f3daede-00-1452hpvj5auww.pike.replit.dev/widget.js"
-        data-store="80cdf60b-d73d-4659-b792-a48a3ca07a7e"
-        strategy="afterInteractive"
-      />
+      <script src="https://c36afef3-bd1c-41d8-9c28-4cc63f3daede-00-1452hpvj5auww.pike.replit.dev/widget.js" data-store-id="220514a0-115c-43d2-932b-45c8f8aa28fb" data-cart-event="cart-to-whatsapp:updated"></script>
     </CartProvider>
   );
 }
