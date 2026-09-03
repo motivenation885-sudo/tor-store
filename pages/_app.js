f1b109c3861ef23ace8e0848834bbd892c7a6c89
motivenation885-sudo <motivenation885@gmail.com>
2026-09-03 18:41:25 +0530

    widget updated

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,12 @@ export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
       <Component {...pageProps} />
-      <script src="https://c36afef3-bd1c-41d8-9c28-4cc63f3daede-00-1452hpvj5auww.pike.replit.dev/widget.js" data-store-id="220514a0-115c-43d2-932b-45c8f8aa28fb" data-cart-event="cart-to-whatsapp:updated"></script>
+      <Script
+  src="https://c36afef3-bd1c-41d8-9c28-4cc63f3daede-00-1452hpvj5auww.pike.replit.dev/widget.js"
+  data-store-id="220514a0-115c-43d2-932b-45c8f8aa28fb"
+  data-cart-event="cart-to-whatsapp:updated"
+  strategy="afterInteractive"
+/>
     </CartProvider>
   );
 }
